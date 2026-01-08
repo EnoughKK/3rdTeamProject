4ae9dd32ca90a71230f6252d487699f97e762e9d
Yun <shinhwa30724@gmail.com>
2026-01-08 14:08:58 +0900

    [FEAT] monsterupdate #4

--- 3rdTeamProject/Game.cpp
+++ 3rdTeamProject/Game.cpp
@@ -7,6 +7,7 @@ Game::Game()
 	{
 
 	}
+	//
 }
 
 Game::~Game()
@@ -14,6 +15,7 @@ Game::~Game()
 	Release();
 	_CrtDumpMemoryLeaks(); // 메모리 누수 체크 디버그창에 나옴
 	// Tesgin
+	// Issue 
 }
 
 void Game::Initialize()
