d4ad09a90b9f6dd5d738dfc2f376997ed79eccca
Yun <shinhwa30724@gmail.com>
2026-01-08 15:41:59 +0900

    commit Test

--- 3rdTeamProject/Stage_21.cpp
+++ 3rdTeamProject/Stage_21.cpp
@@ -1,33 +1,24 @@
 #include "pch.h"
 #include "Stage_21.h"
 
-Stage_21::Stage_21()
-{
-}
+Stage_21::Stage_21()  { }
+Stage_21::~Stage_21() { Release(); }
 
-Stage_21::~Stage_21()
-{
-}
+void Stage_21::Init() { 
 
-void Stage_21::Init()
-{
 }
-
-void Stage_21::Update()
-{
+void Stage_21::Update() {
     GET_SINGLE(ObjectManager)->Update();
 
 }
 
-void Stage_21::Late_Update()
-{
+void Stage_21::Late_Update() {
     GET_SINGLE(ObjectManager)->Late_Update();
 
 }
 
-void Stage_21::Render(HDC hdc)
-{
-    GET_SINGLE(ObjectManager)->Render(hdc);
+void Stage_21::Render(HDC DC) {
+    GET_SINGLE(ObjectManager)->Render(DC);
 
 }
 
--- 3rdTeamProject/Stage_21.h
+++ 3rdTeamProject/Stage_21.h
@@ -10,7 +10,7 @@ public:
 	virtual void Init() override;
 	virtual void Update() override;
 	virtual void Late_Update() override;
-	virtual void Render(HDC hdc) override;
+	virtual void Render(HDC DC) override;
 	virtual void Release() override;
 };
 
