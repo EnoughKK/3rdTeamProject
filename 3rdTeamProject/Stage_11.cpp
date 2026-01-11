f0752b75e7711e021542528bdfeac70d707e9fe1
EnoughKK <ksu8063@naver.com>
2026-01-11 14:01:44 +0900

    [FEAT] 역삼각형 전까지 맵제작 #28

--- 3rdTeamProject/Stage_11.cpp
+++ 3rdTeamProject/Stage_11.cpp
@@ -23,62 +23,7 @@ void Stage_11::Init()
 		GET_SINGLE(ObjectManager)->Add(PLAYER, player);
 	}
 
-	{
-		Object*SO = new TriAngle_Obstacle;
-		SO->Initialize();
-		SO->SetPos(650.f, 475.f);
-		SO->SetSize(50.f, 50.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
-
-		SO = new Square_Obstacle;
-		SO->Initialize();
-		SO->SetPos(700.f, 475.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
-
-		SO = new Jump_Obstacle;
-		SO->Initialize();
-		SO->SetPos(700.f, 440.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_J, SO);
-
-		SO = new Square_Obstacle;
-		SO->Initialize();
-		SO->SetPos(950.f, 275.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
-
-		SO = new Square_Obstacle;
-		SO->Initialize();
-		SO->SetPos(1200.f, 275.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
-
-		SO = new Jump_Obstacle;
-		SO->Initialize();
-		SO->SetPos(1550.f, 490.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_J, SO);
-
-		SO = new TriAngle_Obstacle;
-		SO->Initialize();
-		SO->SetPos(1600.f, 475.f);
-		SO->SetSize(50.f, 50.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
-
-		SO = new TriAngle_Obstacle;
-		SO->Initialize();
-		SO->SetPos(1650.f, 475.f);
-		SO->SetSize(50.f, 50.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
-
-		SO = new TriAngle_Obstacle;
-		SO->Initialize();
-		SO->SetPos(1700.f, 475.f);
-		SO->SetSize(50.f, 50.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
-
-		SO = new TriAngle_Obstacle;
-		SO->Initialize();
-		SO->SetPos(1750.f, 475.f);
-		SO->SetSize(50.f, 50.f);
-		GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
-	}
+	mapInit();
 }
 
 void Stage_11::Update()
@@ -103,4 +48,235 @@ void Stage_11::Render(HDC hdc)
 
 void Stage_11::Release()
 {
+}
+
+void Stage_11::mapInit()
+{
+	Object* SO = new Jump_Obstacle;
+	SO->Initialize();
+	SO->SetPos(650.f, 490.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_J, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(700.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(750.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(800.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(850.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1200.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1250.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1600.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1650.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1700.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1800.f, 475.f);
+	SO->SetSize(150.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1900.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(1900.f, 425.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
+
+	SO = new Jump_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2200.f, 490.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_J, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2250.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2300.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2350.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+	SO = new TriAngle_Obstacle;
+
+	SO->Initialize();
+	SO->SetPos(2400.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2450.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2450.f, 412.5f);
+	SO->SetSize(50.f, 25.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2500.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2550.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2600.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2900.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(2950.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3000.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3100.f, 475.f);
+	SO->SetSize(150.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3200.f, 450.f);
+	SO->SetSize(50.f, 100.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3300.f, 475.f);
+	SO->SetSize(150.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3400.f, 425.f);
+	SO->SetSize(50.f, 150.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_S, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3450.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3500.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3550.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3900.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(3950.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(4150.f, 475.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new Square_Obstacle;
+	SO->Initialize();
+	SO->SetPos(4350.f, 425.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+	SO = new TriAngle_Obstacle;
+	SO->Initialize();
+	SO->SetPos(4350.f, 375.f);
+	SO->SetSize(50.f, 50.f);
+	GET_SINGLE(ObjectManager)->Add(OBSTACLE_T, SO);
+
+
 }
